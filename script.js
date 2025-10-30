23ee1975c73dfaf2db017cc7a2f8f50a45bacc1b
subh3ndu <subhendukarmakar61@gmail.com>
2025-10-30 19:31:50 +0530

    Destination added: Simlipal

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 // Handles loading the events for <model-viewer>'s slotted progress bar
-const onProgress = (event) => {
-  const progressBar = event.target.querySelector('.progress-bar');
-  const updatingBar = event.target.querySelector('.update-bar');
+
+document.querySelector("model-viewer").addEventListener("progress", (event) => {
+  const progressBar = event.target.querySelector(".progress-bar");
+  const updatingBar = event.target.querySelector(".update-bar");
   updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
   if (event.detail.totalProgress === 1) {
-    progressBar.classList.add('hide');
-    event.target.removeEventListener('progress', onProgress);
+    progressBar.classList.add("hide");
+    event.target.removeEventListener("progress", onProgress);
   } else {
-    progressBar.classList.remove('hide');
+    progressBar.classList.remove("hide");
   }
-};
-document.querySelector('model-viewer').addEventListener('progress', onProgress);
+});
+
+// handles error
+
+modelViewer.addEventListener("error", (event) => {
+  const errorMessage = document.querySelector("#error-message");
+  const modelViewer = document.querySelector("model-viewer");
+
+  console.error("Model loading error:", event.detail);
+  errorMessage.style.display = "block";
+
+  // You can also hide the model viewer
+  modelViewer.style.display = "none";
+});
